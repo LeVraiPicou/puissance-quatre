b0be0e5d4dbc2203fbc75c342a275c7b5977f031
FERRETE Lucas <f21222681@V-PP-47-140.salsa.univ-amu.fr>
2023-10-10 15:22:15 +0200

    fenêtre de base

--- puissance4.py
+++ puissance4.py
@@ -1,3 +1,13 @@
-print(4)
+import pygame, sys
 
-
+pygame.init()
+screen = pygame.display.set_mode((640, 480))
+pygame.display.set_caption("Puissance 4")
+while True:
+    for event in pygame.event.get():
+        print(event)
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    screen.fill((255,0,0))
+    pygame.display.update()
